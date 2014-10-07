82cf5bf65d7708188b5369fd6d6939b463ae5414
Kevin <kevin.pheasey@gmail.com>
2014-10-07 13:38:55 -0400

    Make sure only window/document eventListeners created by the rails_script application are destroyed during page changes.  Adding a namespace to window or document events will prevent them from being destroyed.

--- lib/rails_script/loader_helper.rb
+++ lib/rails_script/loader_helper.rb
@@ -3,17 +3,7 @@ module RailsScript
 
     def include_rails_script
       javascript_tag <<-RUBY
-  jQuery(function() {
-    window.$this = new (App.#{ controller_path.split(/\/|_/).map(&:capitalize).join('') } || App.Base)();
-    if (typeof $this.#{ action_name } === 'function') {
-      return $this.#{ action_name }.call();
-    }
-  });
-
-  jQuery(document).on('page:before-change', function() {
-    jQuery(document).off();
-    jQuery(window).off();
-  });
+  jQuery(function(){window.$this=new(App.#{controller_path.split(/\/|_/).map(&:capitalize).join('')}||App.Base);if(typeof $this.#{action_name}==="function"){return $this.#{action_name}.call()}});jQuery(document).on("page:before-change",function(){var e,t,n,r,i,s,o,u;o=[window,document];u=[];for(i=0,s=o.length;i<s;i++){e=o[i];u.push(function(){var i,s;i=jQuery._data(e,"events");s=[];for(r in i){n=i[r];s.push(function(){var i,s,o;o=[];for(i=0,s=n.length;i<s;i++){t=n[i];if(t==null){continue}o.push(t.namespace===""?$(e).off(r,t.handler):void 0)}return o}())}return s}())}return u})
       RUBY
     end
 
