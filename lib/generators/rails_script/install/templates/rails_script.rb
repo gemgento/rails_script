8c0eaabd8c22df0ebe02f3a9d7e7db449df07fab
Kevin Pheasey <kevin@kpheasey.com>
2016-04-27 15:38:26 -0400

    Updates to allow RailsScript to work with Turbolinks 5.

--- lib/generators/rails_script/install/templates/rails_script.rb
+++ lib/generators/rails_script/install/templates/rails_script.rb
@@ -1,4 +1,4 @@
 RailsScript.config do |c|
   # Configure name that will be used to namespace the javascript classes
-  c.app_namespace = "App"
+  c.app_namespace = 'App'
 end
--- lib/rails_script/loader_helper.rb
+++ lib/rails_script/loader_helper.rb
@@ -2,23 +2,7 @@ module RailsScript
   module LoaderHelper
 
     def include_rails_script
-      javascript_tag <<-RUBY
-window.Utility || (window.Utility = {});
-Utility.RailsVars = #{@to_javascript.nil? ? '{}' : @to_javascript.to_json};
-
-(function() {
-  window.$this = new (#{RailsScript.app_namespace}.#{ controller_path.split(/\/|_/).map(&:capitalize).join('') } || #{RailsScript.app_namespace}.Base)();
-  if (typeof $this.beforeAction === 'function') {
-    $this.beforeAction("#{action_name}");
-  }
-  if (typeof $this.#{ action_name } === 'function') {
-    $this.#{ action_name }();
-  }
-  if (typeof $this.afterAction === 'function') {
-    $this.afterAction("#{action_name}");
-  }
-})();
-      RUBY
+      content_tag :div, nil, id: 'rails-script', data: { controller: controller_path.split(/\/|_/).map(&:capitalize).join('') }
     end
 
   end
